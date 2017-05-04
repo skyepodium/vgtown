f3a47241342b2b7cc508c07b4acabf8192085089
김정윤 <skyepodium@gimjeong-yun-ui-MacBook-Pro.local>
2017-05-04 23:56:22 +0900

    url2

--- vain/vain/urls.py
+++ vain/vain/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 #    url(r'^admin/', admin.site.urls),
 #    url(r'^admin/', admin.site.urls),    
+#
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('web.urls', namespace='web')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
